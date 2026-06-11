153ccf30e9c42396962f423626a1a6c20e1b5f1b
Ethan Brady <turing@itsEthan.dev>
2026-06-11 00:39:47 -0600

    Copied over nixos defaults

--- systems/x86_64-linux/mason/default.nix
+++ systems/x86_64-linux/mason/default.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -13,5 +13,51 @@ _: {
     system = "x86_64-linux";
   };
 
-  networking.hostName = "mason";
+  networking = {
+    hostName = "mason";
+    networkmanager.enable = true;
+  };
+
+  boot = {
+    loader.grub.enable = true;
+    loader.grub.device = "/dev/nvme0n1";
+    loader.grub.useOSProber = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  services = {
+    xserver.enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+    printing.enable = true;
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+
+  users.users."masons" = {
+    isNormalUser = true;
+    description = "Mason Shaffer";
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  system.stateVersion = "26.05";
 }
